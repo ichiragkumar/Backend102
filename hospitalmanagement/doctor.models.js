9c6e019b619ba6431e0a12251de028fe346e0dae
ichiragkumar <chiragroy3030@gmail.com>
2024-06-15 22:54:21 +0530

    Completed Hospital Models

--- hospitalmanagement/doctor.models.js
+++ hospitalmanagement/doctor.models.js
@@ -1,6 +1,44 @@
 import mongoose from "mongoose"
 
+const doctorsHoursinMultipleHospital = new mongoose.Schema({
+    hoursWorkd:{
+        type:Number,
+        required:true
+    },
+    patientManaged:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Patient"
+    }
+})
+
 const doctorsSchema = new mongoose.Schema({
+
+    name:{
+        type:String,
+        required:true,
+    },
+    salary:{
+        type:String,
+        required:true,
+    },
+    qualification:{
+        type:String,
+        required:true,
+    },
+    exeprienceinYear:{
+        type:Number,
+        default:0,
+    },
+    worksInHospital:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Hospital"
+        },
+        doctorsHoursinMultipleHospital,
+        
+
+    ]
+
 
 }, {timestamps:true})
 
